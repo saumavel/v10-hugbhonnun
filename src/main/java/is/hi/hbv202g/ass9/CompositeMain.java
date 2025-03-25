069188fe61dab53b489c5b5172920065d94b1e80
Ásdís Valtýsdóttir <AsdisVal@users.noreply.github.com>
2025-03-25 10:39:01 +0000

    comment um main fallið

--- src/main/java/is/hi/hbv202g/ass9/CompositeMain.java
+++ src/main/java/is/hi/hbv202g/ass9/CompositeMain.java
@@ -3,7 +3,12 @@ package is.hi.hbv202g.ass9;
 import is.hi.hbv202g.ass9.composite.*;
 
 public class CompositeMain {
-
+	/**
+	 * Demonstrates the composite pattern by calculating and printing the result of
+	 * (1+2)*4.
+	 * 
+	 * @param args to pass arguments
+	 */
 	public static void main(String[] args) {
 		NumberLeaf number1 = new NumberLeaf(1);
 		NumberLeaf number2 = new NumberLeaf(2);
@@ -17,5 +22,7 @@ public class CompositeMain {
 		multiplyComposite.add(number4);
 		multiplyComposite.add(plusComposite);
 
-		System.out.println("The result of ("+number1.getResult()+"+"+number2.getResult()+")*"+number4.getResult()+" is: "+ multiplyComposite.getResult());	}
+		System.out.println("The result of (" + number1.getResult() + "+" + number2.getResult() + ")*"
+				+ number4.getResult() + " is: " + multiplyComposite.getResult());
+	}
 }
